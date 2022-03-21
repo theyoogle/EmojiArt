9f766a963a4b3cab94603537b1324f9d25fb5f58
YooGle <yooglesharma@gmail.com>
2022-03-21 21:04:36 +0530

    user defaults

---
+++ EmojiArt/Model/Palette.swift
@@ -0,0 +1,12 @@
+//
+//  Palette.swift
+//  EmojiArt
+//
+//  Created by The YooGle on 21/03/22.
+//
+
+struct Palette: Identifiable, Codable {
+    var name: String
+    var emojis: String
+    var id: Int
+}
